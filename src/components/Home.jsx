ecd8a30a4976677beb46fe627ef83f6c85e2dba6
Sander Nicolaysen <nicolaysensander96@gmail.com>
2022-09-22 15:58:35 +0200

    fix navbar layout on mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,31 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import PersonCoding from '../assets/person-coding.svg'
-import { HiOutlineMail } from 'react-icons/hi'
-import ArrowDownButton from './ui/ArrowDownButton'
-import { motion } from 'framer-motion'
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import PersonCoding from '../assets/person-coding.svg';
+import { HiOutlineMail } from 'react-icons/hi';
+import ArrowDownButton from './ui/ArrowDownButton';
+import { motion } from 'framer-motion';
+import PersonCodingAnimated from './ui/PersonCodingAnimated';
 
 const Home = () => {
   return (
-    <div name="home" className="flex items-center justify-center flex-col container mx-auto min-h-screen pt-20">
-      <motion.img className="mb-4" src={PersonCoding} alt="SVG icon of person coding" />
+    <div
+      name="home"
+      className="flex items-center justify-center flex-col container mx-auto min-h-screen pt-20"
+    >
+      <div className="mb-4">
+        <PersonCodingAnimated />
+      </div>
       <h1 className="text-4xl font-bold text-center uppercase">
         SANDER <span className="text-green-500">NICOLAYSEN</span>
       </h1>
       <p className="uppercase font-bold text-xl mb-4">Fullstack Developer</p>
       <p className="max-w-xl mb-8 text-center">
-        I’m a full-stack developer specializing in building (and occasionally designing) exceptional digital
-        experiences. Currently, I’m focused on building responsive full-stack web applications.
+        {/* I’m a full-stack developer specializing in building (and occasionally
+        designing) exceptional digital experiences. Currently, I’m focused on
+        building responsive full-stack web applications. */}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui
+        repellat ex facilis obcaecati saepe, corporis itaque deleniti odio id.
+        Quod cupiditate consequuntur veniam explicabo minus reiciendis voluptate
+        saepe praesentium.
       </p>
       <div className="flex gap-3 mb-16">
         <motion.a
@@ -44,7 +55,7 @@ const Home = () => {
       </div>
       <ArrowDownButton />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,32 @@
-import { Link } from 'react-scroll'
-import ThemeToggle from './ui/ThemeToggle'
-import { FaPaw } from 'react-icons/fa'
-import { useEffect, useState } from 'react'
-import Hamburger from './ui/Hamburger'
-import { motion } from 'framer-motion'
+import { Link } from 'react-scroll';
+import ThemeToggle from './ui/ThemeToggle';
+import { FaPaw } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import Hamburger from './ui/Hamburger';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  console.log(isOpen)
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
 
   const handleHamburgerToggle = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
       <nav
-        className={`fixed z-20 w-full top-0 left-0 h-20 items-center flex px-6 justify-between backdrop-blur text-white text-2xl bg-darkGray-500 dark:bg-secondary`}
+        className={`fixed z-20 w-full top-0 left-0 h-20 items-center flex px-6 md:px-12 justify-between backdrop-blur text-white text-2xl bg-darkGray-500 dark:bg-secondary`}
       >
-        <Link className={`group cursor-pointer flex items-center gap-2`} to="home" smooth={true} duration={500}>
-          <i className="group-hover:rotate-45 transition-all duration-500 mb-[5px]">{<FaPaw />}</i>
+        <Link
+          className={`group cursor-pointer flex items-center gap-2`}
+          to="home"
+          smooth={true}
+          duration={500}
+        >
+          <i className="group-hover:rotate-45 transition-all duration-500 mb-[5px]">
+            {<FaPaw />}
+          </i>
           <h1 className="font-bold sm:text-2xl animated-b-border">
             S<span className="text-green-500">N</span>
           </h1>
@@ -27,39 +34,67 @@ const Navbar = () => {
         <div className="flex md:hidden">
           <Hamburger isOpen={isOpen} onToggle={handleHamburgerToggle} />
         </div>
+        {isOpen && (
+          <div className="md:ml-16 absolute right-6 top-28 z-10">
+            <ThemeToggle />
+          </div>
+        )}
         <ul
           className={`${
-            isOpen ? 'flex flex-col items-center justify-around h-screen' : 'hidden md:flex md:items-center'
+            isOpen
+              ? 'fixed bg-darkGray-500 top-0 left-0 pt-5 mt-20 h-[calc(100vh-5rem)] w-full flex flex-col items-center justify-around px-6 border-t-2 border-gray-500'
+              : 'hidden md:flex md:items-center'
           }`}
         >
           <li className="">
-            <Link className="animated-b-border" to="home" smooth={true} duration={500}>
+            <Link
+              className="animated-b-border"
+              to="home"
+              smooth={true}
+              duration={500}
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link className="animated-b-border" to="skills" smooth={true} duration={500}>
+            <Link
+              className="animated-b-border"
+              to="skills"
+              smooth={true}
+              duration={500}
+            >
               Skills
             </Link>
           </li>
           <li>
-            <Link className="animated-b-border" to="work" smooth={true} duration={500}>
+            <Link
+              className="animated-b-border"
+              to="work"
+              smooth={true}
+              duration={500}
+            >
               Work
             </Link>
           </li>
           <li>
-            <Link className="animated-b-border" to="contact" smooth={true} duration={500}>
+            <Link
+              className="animated-b-border"
+              to="contact"
+              smooth={true}
+              duration={500}
+            >
               Contact
             </Link>
           </li>
-          <div className="md:ml-16">
-            <ThemeToggle />
-          </div>
+          {!isOpen && (
+            <div className="md:ml-16">
+              <ThemeToggle />
+            </div>
+          )}
         </ul>
       </nav>
-      <div className={`${isOpen ? 'block' : 'hidden'} bg-darkGray-500 h-screen`}></div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
