165870b556b1661f28211a04c7944dd62f97bf1a
Sander Nicolaysen <nicolaysensander96@gmail.com>
2022-09-23 13:09:52 +0200

    improve theme toggle size and animation

--- src/components/ui/ThemeToggle.jsx
+++ src/components/ui/ThemeToggle.jsx
@@ -1,30 +1,47 @@
-import { FaMoon } from 'react-icons/fa'
-import { BsFillSunFill } from 'react-icons/bs'
-import { ThemeContext } from '../context/ThemeContext'
-import { useContext } from 'react'
+import { FaMoon } from 'react-icons/fa';
+import { BsFillSunFill } from 'react-icons/bs';
+import { ThemeContext } from '../context/ThemeContext';
+import { useContext } from 'react';
+import { motion } from 'framer-motion';
+
+const spring = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30,
+};
 
 const Toggle = () => {
-  const { theme, setTheme } = useContext(ThemeContext)
+  const { theme, setTheme } = useContext(ThemeContext);
 
   return (
     <div className="flex items-center justify-between gap-2">
       <span className="text-sm font-medium dark:text-gray-300">
-        {<BsFillSunFill className="animate-spin-slow" color="#FBBE24" />}
+        {
+          <BsFillSunFill
+            className="animate-spin-slow"
+            color="#FBBE24"
+            size={20}
+          />
+        }
       </span>
-      <label htmlFor="default-toggle-size" className="inline-flex relative items-center cursor-pointer">
-        <input
-          type="checkbox"
-          checked={theme === 'light' ? false : true}
-          onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-          value=""
-          id="default-toggle-size"
-          className="sr-only peer"
-        />
-        <div className="w-11 h-6 bg-white peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-darkGray-500 after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500 after:hover:scale-90"></div>
-      </label>
-      <span className="text-sm font-medium">{<FaMoon color="#BEDBFE" />}</span>
+      <div
+        className={`w-11 h-7 bg-white rounded-full p-1 items-center flex cursor-pointer ${
+          theme === 'light' ? 'justify-start' : 'justify-end'
+        }`}
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      >
+        <motion.div
+          className="w-5 h-5 bg-darkGray-500 rounded-full"
+          whileHover={{ scale: 0.8 }}
+          layout
+          transition={spring}
+        ></motion.div>
+      </div>
+      <span className="text-sm font-medium">
+        {<FaMoon color="#BEDBFE" size={20} />}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Toggle
+export default Toggle;
