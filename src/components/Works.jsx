727985b80f02dbf7411645633e8961da47f7ff46
Sander Nicolaysen <nicolaysensander96@gmail.com>
2022-09-27 19:13:46 +0200

    add pictures

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,6 +2,7 @@ import Card from './ui/Card';
 import Flytoget from '../assets/flytoget.png';
 import Leo from '../assets/leo.png';
 import Space from '../assets/space-exploration-game.png';
+import Mario from '../assets/mario.png';
 
 const Works = () => {
   return (
@@ -11,11 +12,15 @@ const Works = () => {
       </h2>
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
         <li className="text-center mb-4">
-          <a href="#" className="">
+          <a
+            href="https://minside.flytoget.no/en/login"
+            target="_blank"
+            className=""
+          >
             <img
               src={Flytoget}
               alt=""
-              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+              className="block object-cover object-center w-full rounded-lg"
             />
           </a>
         </li>
@@ -24,16 +29,20 @@ const Works = () => {
             <img
               src={Leo}
               alt=""
-              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[3/2]"
             />
           </a>
         </li>
         <li className="text-center mb-4 w-full">
-          <a href="#" className="">
+          <a
+            href="https://github.com/SanderNicolaysen/binkbik"
+            className=""
+            target="_blank"
+          >
             <img
               src={Space}
               alt=""
-              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[3/2]"
             />
           </a>
         </li>
@@ -42,7 +51,16 @@ const Works = () => {
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
               alt=""
-              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[3/2]"
+            />
+          </a>
+        </li>
+        <li className="text-center mb-4">
+          <a href="https://github.com/SanderNicolaysen/SAM" target="_blank">
+            <img
+              src={Mario}
+              alt=""
+              className="block object-cover w-full h-full rounded-lg aspect-[3/2]"
             />
           </a>
         </li>
@@ -51,16 +69,7 @@ const Works = () => {
             <img
               src={Leo}
               alt=""
-              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
-            />
-          </a>
-        </li>
-        <li className="text-center mb-4">
-          <a href="#">
-            <img
-              src={Leo}
-              alt=""
-              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[3/2]"
             />
           </a>
         </li>
