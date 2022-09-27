8da6fa3aac099650f5313a651e5df1439c48966b
Sander Nicolaysen <nicolaysensander96@gmail.com>
2022-09-27 14:41:47 +0200

    add grid for projects worked on

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,51 +1,72 @@
-import Card from './ui/Card'
-import Flytoget from '../assets/flytoget.png'
-import Leo from '../assets/leo.png'
+import Card from './ui/Card';
+import Flytoget from '../assets/flytoget.png';
+import Leo from '../assets/leo.png';
+import Space from '../assets/space-exploration-game.png';
 
 const Works = () => {
   return (
-    <section name="works" className="max-w-2xl container mx-auto px-4 sm:px-0">
-      <h2 className="font-semibold text-xl dark:text-white mb-4 border-b-2 dark:border-darkGray-500">Works</h2>
+    <section name="works" className="max-w-3xl container mx-auto px-4">
+      <h2 className="font-semibold text-xl dark:text-white mb-4 border-b-2 dark:border-darkGray-500">
+        Works
+      </h2>
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
         <li className="text-center mb-4">
-          <a href="#">
-            <img src={Flytoget} alt="" className="rounded-xl border border-gray-800" />
-            <div className="mt-3">
-              <h3 className="font-medium text-white">Flytoget</h3>
-              <p>React, Javascript, HTML, CSS, Bootstrap</p>
-            </div>
+          <a href="#" className="">
+            <img
+              src={Flytoget}
+              alt=""
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+            />
           </a>
         </li>
         <li className="text-center mb-4">
           <a href="#">
-            <img src={Leo} alt="" className="rounded-xl border border-gray-800 h-96 md:h-48" />
-            <div className="mt-3">
-              <h3 className="font-medium text-white">Leo</h3>
-              <p>Vue, Node, Express, MongoDB</p>
-            </div>
+            <img
+              src={Leo}
+              alt=""
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+            />
+          </a>
+        </li>
+        <li className="text-center mb-4 w-full">
+          <a href="#" className="">
+            <img
+              src={Space}
+              alt=""
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+            />
           </a>
         </li>
         <li className="text-center mb-4">
           <a href="#">
-            <img src={Flytoget} alt="" className="rounded-xl border border-gray-800" />
-            <div className="mt-3">
-              <h3 className="font-medium text-white">Flytoget</h3>
-              <p>React, Javascript, HTML, CSS, Bootstrap, Redux, Formik</p>
-            </div>
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+              alt=""
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+            />
           </a>
         </li>
         <li className="text-center mb-4">
           <a href="#">
-            <img src={Flytoget} alt="" className="rounded-xl border border-gray-800" />
-            <div className="mt-3">
-              <h3 className="font-medium text-white">Flytoget</h3>
-              <p>React, Javascript, HTML, CSS, Bootstrap, Redux, Formik</p>
-            </div>
+            <img
+              src={Leo}
+              alt=""
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+            />
+          </a>
+        </li>
+        <li className="text-center mb-4">
+          <a href="#">
+            <img
+              src={Leo}
+              alt=""
+              className="block object-cover object-center w-full h-full rounded-lg aspect-[6/4]"
+            />
           </a>
         </li>
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
