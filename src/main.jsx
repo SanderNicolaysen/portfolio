cf46f7a6277992ae393ecd0dc3a5e0aa03a50711
Sander Nicolaysen <nicolaysensander96@gmail.com>
2022-10-06 12:53:19 +0200

    add firebase-tools v.11.13 to try to fix deploy error

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './components/context/ThemeContext';
 import './index.css';
-import { getAnalytics } from 'firebase/analytics';
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from './firebaseConfig';
-
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
