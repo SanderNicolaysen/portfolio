c053ba0fff50c24716d49184bac8cbc6054039d8
Sander Nicolaysen <sander.nicolaysen@tietoevry.com>
2022-10-09 11:47:54 +0200

    remove overflow of image in card on hover

--- src/pages/Work/Card.jsx
+++ src/pages/Work/Card.jsx
@@ -16,10 +16,14 @@ function Card({
 }) {
   return (
     <motion.figure
-      className="group w-full relative mb-4 border border-gray-200 dark:border-darkGray-700 rounded-lg"
+      className="group w-full relative mb-4 border border-gray-200 dark:border-darkGray-700 rounded-lg card-shadow"
       variants={variants}
     >
-      <img src={image} alt={alt} className="h-full w-full rounded-lg" />
+      <img
+        src={image}
+        alt={alt}
+        className="h-full w-full rounded-lg group-hover:opacity-0 transition-opacity duration-500"
+      />
       <figcaption className="rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col justify-between bg-darkGray-500 absolute top-0 left-0 w-full h-full z-10 p-4">
         <div className="flex justify-between items-center">
           <p className="text-xs text-darkgraymuted-500">{year}</p>
